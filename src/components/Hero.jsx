a0961c23492ef119126b15eb872a2c155acc9d4e
Adonis <adonis.casado@outlook.com>
2024-08-24 23:29:49 -0400

    first commit

---
+++ src/components/Hero.jsx
@@ -0,0 +1,12 @@
+
+const Hero = () => {
+  return (
+    <section className="w-full nav-height bg-black relative">
+      <div className="h-5/6">
+
+      </div>
+    </section>
+  )
+}
+
+export default Hero
